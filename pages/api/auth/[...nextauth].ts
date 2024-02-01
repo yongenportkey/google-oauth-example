468696a0034d1692497e4a025efb828bf600a8f9
Loong Yong En <loong@portkey.finance>
2024-02-01 17:01:11 +0800

    feat: persist token

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,6 +10,11 @@ export default NextAuth({
   ],
   callbacks: {
     async jwt({ token, account, profile }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+
       console.log(token);
 
       return token;
