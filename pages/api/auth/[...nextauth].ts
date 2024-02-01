e974c34a39360131455348ac33faad987a2483c7
Loong Yong En <loong@portkey.finance>
2024-02-01 22:15:38 +0800

    feat: remove unused

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { getToken } from "next-auth/jwt";
-
-const secret = process.env.NEXTAUTH_SECRET;
 
 export default NextAuth({
   providers: [
